6b618c9faf53f2396850255cdb595640028fe39a
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2021-10-17 13:09:50 +0700

    create table configuration

---
+++ src/table.config.js
@@ -0,0 +1,26 @@
+module.exports.header = [
+    {
+        value: "Directory",
+        align: "left",
+        headerAlign: "left",
+        width: 40
+    },
+    {
+        value: "Line Coverage",
+        align: "center",
+        width: 20
+    },
+    {
+        value: "Line",
+        align: "center",
+        width: 10
+    }
+]
+
+module.exports.options = {
+    borderStyle: "solid",
+    headerAlign: "center",
+    align: "left",
+    truncate: "...",
+    width: "90%"
+}
